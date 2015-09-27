b08163b5c3bccacc5efa7276acd932aed5e33ea1
Todd Lemoine <todd.lemoine@webtrends.com>
2015-09-27 06:07:50 -0700

    Include testdom, set node version in .nvmrc, tightening up
    Remove fastclick that was included in react-kickstart

--- test/test-init.js
+++ test/test-init.js
@@ -5,6 +5,8 @@ import chai, {
 from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
+import testdom from 'testdom';
+import React from 'react';
 
 chai.use(sinonChai);
 
@@ -12,12 +14,16 @@ global.expect = expect;
 global.assert = assert;
 global.sinon = sinon;
 global.__DEV__ = true;
-global.window = {
-    localStorage: {
-        getItem() { return '[]' },
-        setItem() {}
-    }
+
+testdom('<!doctype html><html><body><main id="root"></main></body></html>');
+
+global.window.localStorage = {
+    getItem() { return '[]' },
+    setItem() {}
 };
+
+global.React = React;
+
 
 // ensure requiring css not to throw
 require.extensions['.css'] = function() {
